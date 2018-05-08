91ff88b911fd312aa3c11f8b8d3487f3af7d15c5
Thiago Gumhold Home <gumi22-_-@hotmail.com>
2018-05-08 13:03:23 +0200

    login works now with db user :D

---
+++ includes/db_connect.php
@@ -0,0 +1,3 @@
+<?php
+include_once 'global-config.php';   // Da functions.php nicht included ist
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
---
+++ includes/global-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Das sind die Login-Angaben für die Datenbank
+ */
+define("HOST", "localhost");     // Der Host mit dem du dich verbinden willst.
+define("USER", "sec_user");    // Der Datenbank-Benutzername.
+define("PASSWORD", "dasi");    // Das Datenbank-Passwort.
+define("DATABASE", "secure_login");    // Der Datenbankname.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", TRUE);    // Weil HTTPS verwendet wird
